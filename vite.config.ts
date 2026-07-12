8a6c02fac3e38a2f81ba526c58fe70091e7e33ac
erichtan2022-cpu <erichtan2022@gmail.com>
2026-07-13 00:10:52 +0700

    Update Vite config for allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
+    allowedHosts: true,
   },
   plugins: [
     react()
